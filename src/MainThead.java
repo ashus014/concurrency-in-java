079e43fa9ff9da07ed14eb6e5d7f7f5a19b7cb2e
ashuM1 <ashu.ask.singh@gmail.com>
2022-05-29 21:35:01 +0530

    (2) Using anonymous class

--- src/MainThead.java
+++ src/MainThead.java
@@ -2,10 +2,22 @@ package src;
 
 public class MainThead {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 
         System.out.println("Hello from Main thread");
         Thread anotherThread = new AnotherThread();
         anotherThread.start();
+        //Thread.sleep(1);
+
+        //creating anonymous Thread Class
+        new Thread(){
+            public void run(){
+                System.out.println("Running from Anonymous Class");
+            }
+        }.start();
+
+
+        System.out.println("Hello from Main thread");
+
     }
 }
